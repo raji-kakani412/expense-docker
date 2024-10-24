7be60cde447632bd028753a68301915242270f3f
rajikakani412 <raji.kakani@gmail.com>
2024-10-24 16:37:15 +0530

    new files

---
+++ install-docker/docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl start docker
+usermod -aG docker ec2-user
+
+# to run these first take sudo access sudo su -
+# go to folder and run command sh docker.sh
